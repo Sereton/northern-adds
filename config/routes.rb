88b4085c724263ab7e65c0f3546f665e63076099
Jay Hernandez <ajuan_jose@hotmail.com>
2019-12-23 19:55:39 -0600

    devise gem added for authentication:Account model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :accounts
+  resources :posts
   resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "public#home"
