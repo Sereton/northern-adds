8ff8f2127949a94ac29b4c51afa110863658e6bf
Jay Hernandez <ajuan_jose@hotmail.com>
2019-12-26 23:32:22 -0600

    futher navigational enhacements, SEO links and CSS fixes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -67,7 +67,7 @@ class CategoriesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_category
-      @category = Category.find(params[:id])
+      @category = Category.find_by_url(params[:url])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
